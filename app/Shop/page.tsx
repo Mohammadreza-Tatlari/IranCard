43ff895c1030e391cf34b6567f7bccc207d2f8ca
Mohammadreza-Tatlari <mohammadreza.tatlari8@gmail.com>
2023-12-18 23:43:58 +0330

    shop page got some modifcation and userState hook is updated

--- app/Shop/page.tsx
+++ app/Shop/page.tsx
@@ -4,8 +4,11 @@ import Shopping from '../components/ShopComponents/Shopping'
 export default function page() {
   return (
     <>
+    <div className="min-h-screen bg-gradient-to-t from-slate-950 to-slate-800">
+    <div className="pt-3 pb-28 ">
     <Shopping />
-    
+    </div>
+    </div>
     </>
   )
 }
--- app/hooks/useUserState.ts
+++ app/hooks/useUserState.ts
@@ -4,16 +4,16 @@ interface useUserStateProps{
     userName: string | undefined,
     isVerified: boolean | undefined;
     userJWT: string | undefined;
-    onSetUserName: (data:string) => void;
-    onSetVerified: (data:boolean) => void;
+    onSetUserName: (data:string | undefined) => void;
+    onSetVerified: (data:boolean | undefined) => void;
 }
 
 const useUserState = create<useUserStateProps>((set) => ({
     userName: Cookies.get('userName'),
     isVerified: Cookies.get('needToVerify') === 'true',
     userJWT: Cookies.get('JWTToken'),
-    onSetUserName: (userName:string) => set({userName: userName}),
-    onSetVerified: (isVerified:boolean) => set({isVerified: isVerified})
+    onSetUserName: (userName:string | undefined) => set({userName: userName}),
+    onSetVerified: (isVerified:boolean | undefined) => set({isVerified: isVerified})
 }))
 
 export default useUserState;
